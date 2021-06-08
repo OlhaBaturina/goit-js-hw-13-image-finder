a206cdee3f0263d9d11361c9425a006d6bd9dc98
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-06-09 01:01:26 +0300

    Рендерит инфу (иконки)

--- src/js/index.js
+++ src/js/index.js
@@ -16,8 +16,41 @@ function onSearch(evt) {
   const searchQuery = form.value.trim();
 
   console.log(searchQuery);
-
-  fetchImage(searchQuery)
-    .then(r => console.log(r))
-    .catch(console.error);
+  renderRef.innerHTML = '';
+  if (searchQuery === '') {
+    return errorAlert('Empty request, enter your search data');
+  }
+  fetchImage(searchQuery).then(renderImage).catch(console.error);
 }
+
+function renderImage(images) {
+  const cardMarkup = imageCard(images);
+  renderRef.innerHTML = cardMarkup;
+}
+
+// function renderGalery(images) {
+//   const cardMarkup = galleryMarkup(images);
+//   renderRef.innerHTML = cardMarkup;
+// }
+
+// function markup(arrayCountries) {
+//   if (arrayCountries.length === 1) {
+//     renderImage(arrayCountries);
+//   }
+
+//   if (arrayCountries.length > 1 && arrayCountries.length <= 10) {
+//     warningAlert('Please enter a more precise query');
+//     renderCountriesList(arrayCountries);
+//   }
+
+//   if (arrayCountries.length > 10) {
+//     renderRef.innerHTML = '';
+//     warningAlert('Too many matches found. Please enter a more specific query!');
+//   }
+// }
+// webformatURL - ссылка на маленькое изображение для списка карточек
+// largeImageURL - ссылка на большое изображение (смотри пункт 'дополнительно')
+// likes - количество лайков
+// views - количество просмотров
+// comments - количество комментариев
+// downloads - количество загрузок
