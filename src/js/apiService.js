893dc1885044bf9d2583ee49d480c6ce31d6fba4
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-06-08 13:53:24 +0300

    Вносит изменения в шаблон разметки

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,8 +1,10 @@
 import { errorAlert } from './errors';
 
+let pageOfSet = 1;
+
 export default function fetchCountries(searchQuery) {
   return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${номер_страницы}&per_page=12&key=21973303-2099b25a108053c1f3c237a34`,
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageOfSet}&per_page=12&key=21973303-2099b25a108053c1f3c237a34`,
   ).then(response => {
     if (response.ok) {
       return response.json();
--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,10 @@ import _ from 'lodash';
 import { errorAlert, warningAlert } from './errors';
 import fetchCountries from './apiService';
 import imageCard from '../templates/image-card.hbs';
-import inputMarkup from '../templates/input.hbs';
 import galleryMarkup from '../templates/gallery.hbs';
 
 const renderRef = document.querySelector('.js-render');
-const inputRef = document.querySelector('[data-input="searchQuery"]');
+const inputRef = document.querySelector('.search-form');
 
 inputRef.addEventListener('input', _.debounce(onSearch, 1000));
 
