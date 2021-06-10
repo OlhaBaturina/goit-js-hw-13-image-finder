a4c99a00dbc4eca28b964e1de5877b41a49aa0ec
Olha Baturina <olga.baturina.golovastova@gmail.com>
2021-06-10 16:25:24 +0300

    Добавляет оформление кнопок и исправляет баг

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import imageCard from './templates/image-card.hbs';
 
 const renderRef = document.querySelector('.js-render');
 const inputRef = document.querySelector('.search-form');
-// const loadMoreBtnRefRef = document.querySelector('.load-more_button');
 const serchBtnRef = document.querySelector('.search_button');
 
 const loadMoreBtn = new LoadMoreBtn({
@@ -32,6 +31,7 @@ function onSearch(evt) {
 
 function fetchingImages() {
   loadMoreBtn.disable();
+  loadMoreBtn.show();
   newPixabayApi.fetchImage().then(image => {
     renderImageMarkup(image);
     loadMoreBtn.enable();
